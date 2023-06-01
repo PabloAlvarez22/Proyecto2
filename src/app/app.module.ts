acaba43f402d88cd197da3499c3e875fd93e2487
Davis <droldan2019310@kinal.edu.gt>
2023-05-31 19:19:58 -0600

    set new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { RecomendationsComponent } from './components/recomendations/recomendations.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    MyAccountComponent,
+    RecomendationsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
