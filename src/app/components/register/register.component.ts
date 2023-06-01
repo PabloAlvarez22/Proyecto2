b8699cfb8744f6921e5d2aa4e60fb108ed8b213d
Michelle <michellemej@gmail.com>
2023-05-31 19:47:44 -0600

    register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationType, NotificationsService } from 'angular2-notifications';
+import { OPTIONS } from 'src/app/models/ConfigNotifications';
+import { User } from 'src/app/models/User';
+import { RestUserService } from 'src/app/services/restUser/rest-user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public user:User;
+
+  constructor(private router: Router, private notification:NotificationsService, private restUser:RestUserService) { 
+    this.user = new User('','','','',null,'','');
+  }
 
   ngOnInit(): void {
+  }
+
+
+  register(){
+    this.restUser.register(this.user).subscribe((res:any)=>{
+      if(res.find){
+        this.notification.create("Operación exitosa",res.message, NotificationType.Success,OPTIONS, "");
+        this.router.navigateByUrl("");
+      }else{
+        this.notification.create("Error",res.message, NotificationType.Error,OPTIONS, "");
+      }
+    }, error=>{
+      console.log(error);
+      this.notification.create("Error General",error.error.message, NotificationType.Error,OPTIONS, "");
+      
+    })
   }
 
 }
